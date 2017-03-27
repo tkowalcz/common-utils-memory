025b60f0916893c80ee874c17df4453846e80024
Tomasz Kowalczewski <tomasz.kowalczewski@codewise.com>
2017-03-27 13:02:59 +0200

    Re #4: Fix for invalid put impelentation in off heap buffers. Made implementation of native/nonnative off heap buffers more consistent

--- src/test/java/com/codewise/util/memory/FixedOffHeapByteBufferMemoryTest.java
+++ src/test/java/com/codewise/util/memory/FixedOffHeapByteBufferMemoryTest.java
@@ -1,13 +1,39 @@
 package com.codewise.util.memory;
 
+import org.apache.commons.lang3.RandomUtils;
+import org.testng.annotations.Test;
 import sun.nio.ch.DirectBuffer;
 
 import java.nio.ByteBuffer;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class FixedOffHeapByteBufferMemoryTest extends FixedOffHeapMutableMemoryTestBase<FixedOffHeapByteBufferMemory> {
 
     @Override
     protected FixedOffHeapByteBufferMemory wrapByteArray(ByteBuffer memoryBuffer) {
-        return new FixedOffHeapByteBufferMemory(((DirectBuffer)memoryBuffer).address(), memoryBuffer.capacity());
+        return new FixedOffHeapByteBufferMemory(((DirectBuffer) memoryBuffer).address(), memoryBuffer.capacity());
+    }
+
+    @Test
+    public void shouldPutByteArrayToBuffer() {
+        // Given
+        byte[] expected = RandomUtils.nextBytes(128);
+        byte[] clone = expected.clone();
+
+        FixedOffHeapByteBufferMemory buffer = wrapByteArray(ByteBuffer.allocateDirect(112));
+
+        // When
+        buffer.put(4, clone, 20, clone.length - 20);
+
+        // Then
+        assertThat(clone).isEqualTo(expected);
+
+        byte[] actual = new byte[112];
+        buffer.get(0, actual, 0, actual.length);
+
+        for (int i = 0; i < buffer.capacity(); i++) {
+            assertThat(buffer.get(i)).isEqualTo(actual[i]);
+        }
     }
 }
